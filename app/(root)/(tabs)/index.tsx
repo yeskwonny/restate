e9e31abf9a7d84afe451b680f04a3d99e99725f1
yesol <yeahsol.kwon@gmail.com>
2025-03-02 08:30:10 +1000

    home page

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,19 +1,92 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { useEffect } from "react";
+import { router, useLocalSearchParams } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+
+import Search from "@/components/Search";
+import { Card, FeatureCard } from "@/components/Card";
+import Filters from "@/components/Filters";
+import { useGlobalContext } from "@/lib/global-provider";
 
 export default function Index() {
+  const { user } = useGlobalContext();
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="text-red-500 text-[40px] font-rubik">Hello</Text>
-      <Link href="/sign-in">Sign In</Link>
-      <Link href="/profile">pROFILE</Link>
-      <Link href="/properties/1">property</Link>
-    </View>
+    <SafeAreaView>
+      {/* showing all as a header for mini card */}
+      <FlatList
+        data={[1, 2, 3, 4]}
+        numColumns={2}
+        keyExtractor={(item) => item.toString()}
+        contentContainerClassName="pb-32"
+        columnWrapperClassName="flex gap-5 px-5"
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item }) => <Card />}
+        ListHeaderComponent={
+          <View className="px-5">
+            <View className="flex flex-row items-center justify-between mt-5">
+              <View className="flex flex-row">
+                <Image
+                  source={{ uri: user?.avatar }}
+                  className="size-12 rounded-full"
+                />
+                <View className="flex flex-col items-start ml-2 justify-center">
+                  <Text className="text-xs font-rubik text-black-100">
+                    Good Morning
+                  </Text>
+                  <Text className="text-base font-rubik-medium text-black-300">
+                    {user?.name}
+                  </Text>
+                </View>
+              </View>
+              <Image source={icons.bell} className="size-6" />
+            </View>
+            <Search />
+            <View className="my-5">
+              <View className="flex flex-row items-center justify-between">
+                <Text className="text-xl font-rubik-bold text-black-300">
+                  Featured
+                </Text>
+                <TouchableOpacity>
+                  <Text className="text-base font-rubik-bold text-primary-300">
+                    See all
+                  </Text>
+                </TouchableOpacity>
+              </View>
+              {/* main card section */}
+              <FlatList
+                data={[1, 2, 3, 4]}
+                renderItem={({ item }) => <FeatureCard />}
+                keyExtractor={(item) => item.toString()}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                contentContainerClassName="flex gap-5 mt-5"
+                bounces={false}
+              />
+            </View>
+            <View className="flex flex-row items-center justify-between px-4">
+              <Text className="text-xl font-rubik-bold text-black-300">
+                Our Recommendation
+              </Text>
+              <TouchableOpacity>
+                <Text className="text-base font-rubik-bold text-primary-300">
+                  See all
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <Filters />
+          </View>
+        }
+      />
+    </SafeAreaView>
   );
 }
--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,12 +1,117 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  Alert,
+  Image,
+  ImageSourcePropType,
+  SafeAreaView,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+import { logout } from "@/lib/appwrite";
+import { useGlobalContext } from "@/lib/global-provider";
+
+import icons from "@/constants/icons";
+import { settings } from "@/constants/data";
+import images from "@/constants/images";
+
+interface SettingsItemProps {
+  icon: ImageSourcePropType;
+  title: string;
+  onPress?: () => void;
+  textStyle?: string;
+  showArrow?: boolean;
+}
+const SettingsItem = ({
+  icon,
+  title,
+  onPress,
+  textStyle,
+  showArrow = true,
+}: SettingsItemProps) => {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      className="flex flex-row items-center justify-between py-3"
+    >
+      <View className="flex flex-row items-center gap-3">
+        <Image source={icon} className="size-6" />
+        <Text
+          className={`text-lg font-rubik-medium text-black-300 ${textStyle}`}
+        >
+          {title}
+        </Text>
+      </View>
+
+      {showArrow && <Image source={icons.rightArrow} className="size-5" />}
+    </TouchableOpacity>
+  );
+};
 
 const profile = () => {
-  return (
-    <View>
-      <Text>profile</Text>
-    </View>
-  )
-}
+  // getting the global context
+  const { user, refetch } = useGlobalContext();
 
-export default profile
+  const handleLogout = async () => {
+    const result = await logout();
+    if (result) {
+      Alert.alert("Logout", "You have been logged out successfully");
+      // it calls getCurrentUser function to get the updated user data
+      refetch();
+    } else {
+      Alert.alert("Error", "Failed to logout");
+    }
+  };
+  return (
+    <SafeAreaView className="h-full bg-white">
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerClassName="pb-32 px-7"
+      >
+        <View className="flex flex-row items-center justify-between mt-5">
+          <Text className="text-xl font-rubik-bold">Profile</Text>
+          <Image source={icons.bell} className="size-5" />
+        </View>
+        <View className="flex flex-row justify-center mt-5">
+          <View className="flex flex-col items-center relative mt-5">
+            <Image
+              source={{ uri: user?.avatar }}
+              className="size-44 relative rounded-full"
+            />
+            <TouchableOpacity className="absolute bottom-11 right-2">
+              <Image source={icons.edit} className="size-9" />
+            </TouchableOpacity>
+
+            <Text className="text-2xl font-rubik-bold mt-2">{user?.name}</Text>
+          </View>
+        </View>
+
+        <View>
+          <SettingsItem
+            icon={icons.calendar}
+            title="My Bookings"
+          ></SettingsItem>
+          <SettingsItem icon={icons.wallet} title="Payment"></SettingsItem>
+        </View>
+        <View className="flex flex-col mt-5 border-t pt-4 border-primary-200">
+          {settings.slice(2).map((item, index) => {
+            return <SettingsItem key={index} {...item} />;
+          })}
+        </View>
+
+        <View className="flex flex-col mt-5 border-t pt-4 border-primary-200">
+          <SettingsItem
+            icon={icons.logout}
+            title="Logout"
+            textStyle="text-danger"
+            showArrow={false}
+            onPress={handleLogout}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default profile;
